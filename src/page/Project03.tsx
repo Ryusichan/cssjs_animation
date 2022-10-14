90be5fbd4cb52ae775aa3ee49e9631bb2769f518
Ryusichan <Ryusichan@github.com>
2022-10-14 18:30:43 +0900

    project03 making

--- src/page/Project03.tsx
+++ src/page/Project03.tsx
@@ -1,8 +1,36 @@
 import React from "react";
-import ProjectLayout from "../layouts/ProjectLayout";
+import "./Project03.css";
 
 const Project03 = () => {
-  return <ProjectLayout>Project03</ProjectLayout>;
+  const [checked, setChecked] = React.useState(false);
+
+  console.log("checked", checked);
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <input
+          type="checkbox"
+          className={`toggleMe ${checked ? "checked" : ""}`}
+          onClick={() => setChecked(!checked)}
+        />
+        <div className={"circle"}></div>
+        <div className={"circleOther"}></div>
+        <div className={"flexDiv"}>
+          <div></div>
+          <div style={{ transitionDelay: "20ms" }}></div>
+          <div style={{ transitionDelay: "40ms" }}></div>
+          <div style={{ transitionDelay: "60ms" }}></div>
+          <div style={{ transitionDelay: "80ms" }}></div>
+          <div style={{ transitionDelay: "100ms" }}></div>
+          <div style={{ transitionDelay: "120ms" }}></div>
+          <div style={{ transitionDelay: "140ms" }}></div>
+          <div style={{ transitionDelay: "160ms" }}></div>
+          <div style={{ transitionDelay: "180ms" }}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project03;
